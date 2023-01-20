9dcd17ff574a64c0b347237c9dfd4faf8637b0ef
ilisiemarcussilviu <78593775+ilisiemarcussilviu@users.noreply.github.com>
2023-01-20 20:59:06 +0200

    Created main.py
    Created main.py with only one route, "search", with the method "GET"

---
+++ api/criminal-records/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['GET'])
+def echo():
+    full_name = request.args.get('full-name')
+    response = {'full-name': full_name,'status':'wip'}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
