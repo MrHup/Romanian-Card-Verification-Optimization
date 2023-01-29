b867c6e8aa923434e9366375611ef705fef8c689
Marcus <marcus.ilisie99@e-uvt.ro>
2023-01-29 12:17:31 +0200

    modifying imports, adding admin route that manages both login and the admin dashboard

--- api/criminal-records/app.py
+++ api/criminal-records/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ def search():
     full_name = request.args.get('full-name')
     response = {'full-name': full_name,'status':'wip'}
     return jsonify(response)
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'GET':
+        return render_template('admin.html')
+    elif request.method == 'POST':
+        pin_id = request.form['pin-id']
+        pin_key = request.form['pin-key']
+        if pin_id == 'RICVR-23-T' and pin_key == 'T2Y433wrg3ad':
+            return 'Access granted'
+        else:
+            return 'Access denied'
 
 if __name__ == '__main__':
     app.run()
