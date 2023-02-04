4de8318a3a6e6726a21a1fcdd44916fb1ae370e9
Marcus <marcus.ilisie99@e-uvt.ro>
2023-02-04 19:32:27 +0200

    finished update_records route method. added rendered table of screening.db contents.

--- api/criminal-records/app.py
+++ api/criminal-records/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request, render_template, session
 import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 app = Flask(__name__)
 app.secret_key = 'secret-key'
 
@@ -12,6 +15,13 @@ def connect_db():
     conn.commit()
     return conn, c
 
+def connect_screening_db():
+    conn = sqlite3.connect("screening.db")
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS ro_wanted_persons (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, DOB TEXT, extracted_date TIMESTAMP)
+              """)
+    conn.commit()
+    return conn, c
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -34,7 +44,12 @@ def admin():
         searches = c.fetchall()
         print(searches)
         conn.close()
-        return render_template('admin.html', searches=searches)
+
+        conn, c = connect_screening_db()
+        c.execute(""" SELECT * FROM ro_wanted_persons WHERE extracted_date = (SELECT MAX(extracted_date) FROM ro_wanted_persons)""")
+        scr = c.fetchall()
+        conn.close()
+        return render_template('admin.html', searches=searches,scr = scr)
     if request.method == 'GET':
         return render_template('admin_login.html')
     elif request.method == 'POST':
@@ -45,7 +60,37 @@ def admin():
             return render_template('admin.html')
         else:
             return 'Access denied'
+@app.route('/update_records', methods=['GET'])
+def update_records():
+    url = "https://www.politiaromana.ro/ro/persoane-urmarite"
+    response = requests.get(url,verify=False)
+    soup = BeautifulSoup(response.content, "html.parser")
 
+    names = soup.find_all("h3", class_="descNume")
+    names_list = []
+    for name in names:
+        print(name.text)
+        names_list.append(name.text)
+
+    dates_of_birth = soup.find_all("span", class_="descData")
+    dob_list = []
+    for dob in dates_of_birth:
+        print(dob.text)
+        dob_list.append(dob.text)
+
+    conn = sqlite3.connect("screening.db")
+    c = conn.cursor()
+
+    c.execute("DROP TABLE IF EXISTS ro_wanted_persons")
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS ro_wanted_persons (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, DOB TEXT, extracted_date TIMESTAMP)")
+    now = datetime.now()
+    for i in range(len(names_list)):
+        c.execute("INSERT INTO ro_wanted_persons (Name, DOB, extracted_date) VALUES (?, ?, ?)",
+                  (names_list[i], dob_list[i], now))
+    conn.commit()
+    conn.close()
+    return "Records updated"
 
 if __name__ == '__main__':
     app.run()
