82df7c53e104dc83396fdb40921dd05a6c1cf351
Marcus <marcus.ilisie99@e-uvt.ro>
2023-02-04 20:56:11 +0200

    changed the search logs table tile to "All Searches" in admin.html added screening hits to admin.html added connection method for screening_hits table in app.py added mechanism to assess matches between the dataset extracted from the romanian police website by score

--- api/criminal-records/app.py
+++ api/criminal-records/app.py
@@ -3,6 +3,8 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import difflib
+
 app = Flask(__name__)
 app.secret_key = 'secret-key'
 
@@ -23,16 +25,65 @@ def connect_screening_db():
     conn.commit()
     return conn, c
 
+def connect_screening_hits_db():
+    conn = sqlite3.connect("screening.db")
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS screening_hits (ID INTEGER PRIMARY KEY AUTOINCREMENT, API_name TEXT, found_person_name TEXT, found_person_dob TEXT,alert_index FLOAT,hit_date TIMESTAMP)
+              """)
+    conn.commit()
+    return conn, c
+
 @app.route('/search', methods=['GET'])
 def search():
     full_name = request.args.get('full-name')
-    response = {'full-name': full_name,'status':'wip'}
+
+    # read from db names list
+    # if full match, save in searches_hits_misses, alert_index = 1
+    # if partial match, save in searches_hits_misses, alert_index = 0.5
+    # if no match, save in searches_hits_misses, alert_index = 0
+    # response = {'full-name': full_name,'status':'wip'}
     conn, c = connect_db()
     c.execute("""INSERT INTO searches (full_name, timestamp) 
                  VALUES (?, datetime('now'))
               """, (full_name,))
     conn.commit()
     conn.close()
+    conn, c = connect_screening_db()
+    c.execute(""" SELECT * FROM ro_wanted_persons WHERE extracted_date = (SELECT MAX(extracted_date) FROM ro_wanted_persons)""")
+    scr = c.fetchall()
+    conn.close()
+
+    conn,c = connect_screening_hits_db()
+    for result in scr:
+        if result[1]==full_name:
+            response = {'full-name': full_name, 'found_person_name':result[1],'found_person_dob':result[2],'alert_index': 1}
+            c.execute("""INSERT INTO screening_hits (API_name, found_person_name, found_person_dob,alert_index,hit_date) 
+                 VALUES (?, ?,?,?,datetime('now'))
+              """, (full_name,result[1],result[2],1))
+            conn.commit()
+            conn.close()
+            return jsonify(response)
+    for result in scr:
+            if result[1].count(" ")==2:
+                n_list = [result[1],
+                           f"{result[1].split(' ')[0]} {result[1].split(' ')[1]}"]
+            else:
+                n_list=[result[1]]
+
+            match = difflib.get_close_matches(full_name, n_list)
+            if match:
+                response = {'full-name': full_name, 'found_person_name':result[1],'found_person_dob':result[2],'alert_index': 0.5}
+                c.execute("""INSERT INTO screening_hits (API_name, found_person_name, found_person_dob,alert_index,hit_date) 
+                    VALUES (?, ?,?,?,datetime('now'))
+                """, (full_name,result[1],result[2],0.5))
+                conn.commit()
+                conn.close()
+                return jsonify(response)
+    response = {'full-name': full_name, 'found_person_name':'no_result','found_person_dob':'no_result','alert_index': 0}
+
+    conn.close()    
+    
+    print(scr)
     return jsonify(response)
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -49,7 +100,12 @@ def admin():
         c.execute(""" SELECT * FROM ro_wanted_persons WHERE extracted_date = (SELECT MAX(extracted_date) FROM ro_wanted_persons)""")
         scr = c.fetchall()
         conn.close()
-        return render_template('admin.html', searches=searches,scr = scr)
+
+        conn,c = connect_screening_hits_db()
+        c.execute("""SELECT * FROM screening_hits""")
+        screening_hits = c.fetchall()
+        conn.close()
+        return render_template('admin.html', searches=searches,scr = scr,screening_hits=screening_hits)
     if request.method == 'GET':
         return render_template('admin_login.html')
     elif request.method == 'POST':
