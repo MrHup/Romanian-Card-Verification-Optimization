557b588588cb6aa3600adf290bd159e741c0a780
inocentiumoga <72560959+inocentiumoga@users.noreply.github.com>
2023-02-06 00:01:33 +0200

    Update cnp-validation.py

--- api/cnp-validation/cnp-validation.py
+++ api/cnp-validation/cnp-validation.py
@@ -2,3 +2,9 @@ def ID_check(CNP,sex,seria):
     if CNP.isdigit()==0:
         print('1')
         return False
+    if len(CNP)!=13:
+        print('2')
+        return False
+    if int(CNP[0]) < 1 or int(CNP[0]) > 8:
+        print('3')
+        return False
