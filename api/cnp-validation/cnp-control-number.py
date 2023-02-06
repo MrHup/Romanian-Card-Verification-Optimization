811308d7b745e06cbbc7291cf83ac10407043faf
inocentiumoga <72560959+inocentiumoga@users.noreply.github.com>
2023-02-06 20:10:21 +0200

    scripts removal

--- api/cnp-validation/cnp-control-number.py
+++
@@ -1,8 +0,0 @@
-def compute_CN(CNP,FNC='279146358279'):
-    SC = 0 
-    for i in range(12):
-        SC = SC + int(CNP[i]) * int(FNC[i])
-    if SC % 11 < 10:
-        return SC % 11
-    elif SC % 11 == 10:
-        return 1
