186735c4d515f7401aa70476b3404450997d920b
inocentiumoga <72560959+inocentiumoga@users.noreply.github.com>
2023-02-06 18:15:09 +0200

    Update cnp-validation.py

--- api/cnp-validation/cnp-validation.py
+++ api/cnp-validation/cnp-validation.py
@@ -1,4 +1,4 @@
-def ID_check(CNP,sex):
+def ID_check(CNP):
     if CNP.isdigit()==0:
         return False
     if len(CNP)!=13:
@@ -9,7 +9,10 @@ def ID_check(CNP,sex):
         return False
     if int(CNP[5:7]) < 1 or int(CNP[5:7]) > 31:
         return False
-    if seria in Serii == False:
+    if int(CNP[7:9]) < 1 or int(CNP[7:9]) > 48:
+        return False
+    if int(CNP[9:12]) == 0:
         return False
     if compute_CN(CNP) != int(CNP[12]):
         return False
+    return 'CNP Valid'
