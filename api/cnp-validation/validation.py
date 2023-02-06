4723ecb1647b004456c0c8a426265b881e481683
inocentiumoga <72560959+inocentiumoga@users.noreply.github.com>
2023-02-06 23:09:32 +0200

    Update validation.py

--- api/cnp-validation/validation.py
+++ api/cnp-validation/validation.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
-def compute_CN(cnp):
+@app.get("/compute_cn/{cnp}")
+async def compute_cn(cnp):
     fnc='279146358279'
     sc = 0 
     for i in range(12):
@@ -15,19 +15,19 @@ def compute_CN(cnp):
 @app.get("/validation/{cnp}")
 async def validation(cnp):
     if cnp.isdigit()==0:
-        return False
+        return {"success":False, "reason":"The CNP is not numeric."}
     if len(cnp)!=13:
-        return False
+        return {"success":False, "reason":"The CNP does not have an acceptable size."}
     if int(cnp[0]) < 1 or int(cnp[0]) > 8:
-        return False
+        return {"success":False, "reason":"Sex digit not valid."}
     if int(cnp[3:5]) < 1 or int(cnp[3:5]) > 12:
-        return False
+        return {"success":False, "reason":"Birth month digits not valid."}
     if int(cnp[5:7]) < 1 or int(cnp[5:7]) > 31:
-        return False
-    if int(cnp[7:9]) < 1 or int(cnp[7:9]) > 48:
-        return False
+        return {"success":False, "reason":"Birth day not valid."}se
+    if int(cnp[7:9]) < 1 or int(cnp[7:9]) > 52:
+        return {"success":False, "reason":"County code not valid."}
     if int(cnp[9:12]) == 0:
-        return False
-    if compute_CN(cnp) != int(cnp[12]):
-        return False
+        return {"success":False, "reason":"Sequential number not valid."}
+    if compute_cn(cnp) != int(cnp[12]):
+        return {"success":False, "reason":"Control number not valid."}
     return True
